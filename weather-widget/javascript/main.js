91d2f492d94f3a46efa098b59c6e7806149b65e8
Nicoleluu <67698387+Nicoleluu@users.noreply.github.com>
2026-06-26 23:26:04 -0400

    Events and UIs
    Interactive Web submission

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -1,15 +1,30 @@
-// check that the javascript is working
-console.log("this works");
+// grab the input box and the search button from the page
+let input = document.querySelector(".zipcode");
+let btn = document.querySelector(".search-button");
 
-// get my api key from the config file so it stays secret
-const API_KEY = config.WEATHER_API_KEY;
+// this function gets the weather for the zipcode we give it
+getWeatherData = (zip) => {
+  let API_KEY = config.WEATHER_API_KEY;
+  let API_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&APPID=${API_KEY}`;
 
-// this function asks open weather map for the weather and prints it
-function getWeatherData() {
-  fetch(`http://api.openweathermap.org/data/2.5/weather?zip=10128&APPID=${API_KEY}`)
-    .then(response => response.json())
-    .then(data => console.log(data));
-}
+  fetch(API_ENDPOINT)
+    .then((response) => response.json())
+    .then((data) => {
+      // save the weather data in a variable
+      let local_weather_data = data;
+      // show it in the console
+      console.log(local_weather_data);
+    });
+};
 
-// run the function
-getWeatherData()
+// this runs when the search button is clicked
+const getZipCode = (e) => {
+  e.preventDefault();
+  // get what the user typed in the box
+  let ZIP_CODE = input.value;
+  // use that zipcode to get the weather
+  getWeatherData(ZIP_CODE);
+};
+
+// listen for a click on the search button
+btn.addEventListener("click", getZipCode);
