46c48ced7a9fa5ee84be7740942050c77cf645ac
Nicoleluu <67698387+Nicoleluu@users.noreply.github.com>
2026-06-26 22:46:03 -0400

    Data and APIs
    Interactive Web submission

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -1,2 +1,15 @@
-// this file is empty for now
-// we will add the weather code here in the next module
+// check that the javascript is working
+console.log("this works");
+
+// get my api key from the config file so it stays secret
+const API_KEY = config.WEATHER_API_KEY;
+
+// this function asks open weather map for the weather and prints it
+function getWeatherData() {
+  fetch(`http://api.openweathermap.org/data/2.5/weather?zip=10128&APPID=${API_KEY}`)
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
+
+// run the function
+getWeatherData()
